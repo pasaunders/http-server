1cd54a50ea9d9b44e1a621c39aa98e2e154d5d28
pasaunders <kaorti@gmail.com>
2016-12-13 08:46:16 -0800

    tox and setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""Setup file for echo server assignment."""
+
+
+from setuptools import setup
+
+
+setup(
+    name="echo",
+    description="A project to build a basic echo server.",
+    version=0.1,
+    author="Patrick & Rick",
+    author_email="patrick.a.n.saunders@gmail.com",
+    license="MIT",
+    py_modules=['client', 'server'],
+    package_dir={'': 'src'},
+    install_requires=[''],
+    extras_require={'test': ['pytest', 'pytest-watch', 'pytest-cov', 'tox']},
+    entry_points={
+        'console_scripts': [
+            "echo_server = server:main",
+            "echo_client = client:main"
+        ]
+    }
+)
