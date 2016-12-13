0548880adb99a9245d7c06ad4ca5c239597d8f9a
pasaunders <kaorti@gmail.com>
2016-12-12 20:14:41 -0800

    all tests pass individually we need to find out how to keep the server open for the tests all at once.

--- src/client.py
+++ src/client.py
@@ -18,12 +18,25 @@ def client(message):
     #     print(part.decode('utf8'))
     #     if len(part) < buffer_length:
     #         break
-    infos = socket.getaddrinfo('127.0.0.1', 5200)
+    message += 'EOF'
+    print(message)
+    infos = socket.getaddrinfo('127.0.0.1', 5175)
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
 
     client.sendall(message.encode('utf8'))
+
+    # reply = ""
+    # buffer_length = 10
+    # reply_complete = False
+    # while not reply_complete:
+    #     part = client.recv(buffer_length)
+    #     print(part.decode('utf8'))
+    #     if len(part) < buffer_length:
+    #         break
+    # return reply
+    # client.close()
 
     reply = ""
     buffer_length = 10
@@ -34,3 +47,4 @@ def client(message):
         if len(part) < buffer_length:
             break
     return reply
+    client.close()
--- src/server.py
+++ src/server.py
@@ -10,21 +10,43 @@ def server():
         socket.SOCK_STREAM,
         socket.IPPROTO_TCP
     )
-    address = ('127.0.0.1', 5200)
+    address = ('127.0.0.1', 5175)
     server.bind(address)
     server.listen(1)
     conn, addr = server.accept()
 
     # import pdb; pdb.set_trace()
 
+    # message = ""
+    # buffer_length = 10
+    # message_complete = False
+    # while not message_complete:
+    #     part = " "
+    #     part = conn.recv(buffer_length)
+    #     print(part)
+    #     message += part.decode('utf8')
+    #     if len(part) < buffer_length:
+    #         print('loop broke')
+    #         break
+    # print(message)
+    # conn.sendall(message.encode('utf8'))
+    # conn.close()
+
+
+
     message = ""
     buffer_length = 10
     message_complete = False
     while not message_complete:
         part = conn.recv(buffer_length)
-        print(part)
         message += part.decode('utf8')
-        if len(part) < buffer_length:
+        if message[-3:] == 'EOF':
+            return_message = message[:-3]
+            print('eof broke')
             break
-
-    conn.sendall(message.encode('utf8'))
+        print(part)
+        # if len(part) < buffer_length:
+        #     print('loop broke')
+        #     break
+    conn.sendall(return_message.encode('utf8'))
+    conn.close()
