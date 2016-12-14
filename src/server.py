8d7fb36962981a2c6faec57b9ca986da00825658
pasaunders <kaorti@gmail.com>
2016-12-14 15:38:05 -0800

    begun step2 assignment

--- src/server.py
+++ src/server.py
@@ -22,8 +22,6 @@ def server():
             while total_message.count("\r\n\r\n") != 2:
                 part = conn.recv(buffer_length)
                 total_message += part.decode('utf8')
-            total_message = total_message.split("\r\n\r\n")
-            msg_body = total_message[1]
             try:
                 final_message = response_ok() + "".join(msg_body)
                 print(final_message, type(final_message))
@@ -33,7 +31,28 @@ def server():
         except KeyboardInterrupt:
             break
     conn.close()
+    server.shutdown()
     server.close()
+
+
+# def parse_request(total_message):
+#     """Parse user reqest, return error or request URI."""
+#         total_message = total_message.split("\r\n\r\n")
+#         msg_head = total_message[0]
+#         msg_body = total_message[1]
+#         try:
+#             if 'GET' not in msg_head:
+#                 raise ValueError
+#             elif 'HTTP/1.1' not in msg_head:
+#                 raise ValueError
+#             elif  'www.example.com' not in msg_head:
+#                 raise ValueError
+#             elif  'GET /index.html HTTP/1.1\r\nHost: www.example.com' not in msg_head:
+#                 raise ValueError
+#         except ValueError:
+#             return 'improperly'
+#         else
+#             return
 
 
 def response_ok():
--- src/step1.py
+++
@@ -1,6 +0,0 @@
-"""A simple HTTP server."""
-
-
-def response_ok():
-    """Testing for 200 response code."""
-    pass
