a6d585d29062867c3e1913b28fc539773f38d596
pasaunders <kaorti@gmail.com>
2016-12-13 17:32:55 -0800

    tests updated

--- src/test_servers.py
+++ src/test_servers.py
@@ -1,26 +1,38 @@
-"""Test client and echo server."""
+"""Test client and response server."""
+
+
+# def test_one():
+#     """Test if a string of less than one buffer echoes."""
+#     from client import client
+#     assert client(u'less') == u'less'
+
+
+# def test_multiple():
+#     """Test if a string of multiple buffer lengths echoes."""
+#     from client import client
+#     lots_of_words = u'this long string uses multiple buffer lengths'
+#     assert client(lots_of_words) == lots_of_words
+
+
+# def test_exact():
+#     """Test if a string that is an exact multiple of buffer length echoes."""
+#     from client import client
+#     assert client(u'exactlyoneexactly') == u'exactlyoneexactly'
+
+
+# def test_nonascii():
+#     """Test if a string of non-ascii characters echoes."""
+#     from client import client
+#     assert client(u'ΞΞΞΞΞΞΞ') == u'ΞΞΞΞΞΞΞ'
 
 
 def test_one():
-    """Test if a string of less than one buffer echoes."""
-    from client import client
-    assert client(u'less') == u'less'
+    """Test that the server returns 200 OK."""
+    from server import response_ok
+    assert response_ok() == 'HTTP/1.1 200 OK<CRLF>'
 
 
-def test_multiple():
-    """Test if a string of multiple buffer lengths echoes."""
-    from client import client
-    lots_of_words = u'this long string uses multiple buffer lengths'
-    assert client(lots_of_words) == lots_of_words
-
-
-def test_exact():
-    """Test if a string that is an exact multiple of buffer length echoes."""
-    from client import client
-    assert client(u'exactlyoneexactly') == u'exactlyoneexactly'
-
-
-def test_nonascii():
-    """Test if a string of non-ascii characters echoes."""
-    from client import client
-    assert client(u'ΞΞΞΞΞΞΞ') == u'ΞΞΞΞΞΞΞ'
+def test_two():
+    """Test that the server retruns 500 on error."""
+    from server import response_error
+    assert response_error() == 'HTTP/1.1 500 Internal Server Error<CRLF>'
