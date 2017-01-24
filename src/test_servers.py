68d6c030067a82907656a3355e0d2e68e40d2267
Rick Valenzuela <devnull@rickv.com>
2017-01-24 05:52:32 -0800

    added encoding to test file

--- src/test_servers.py
+++ src/test_servers.py
@@ -1,26 +1,29 @@
-"""Test client and echo server."""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+"""Test client and echo server."""
+from __future__ import unicode_literals
 
 def test_one():
     """Test if a string of less than one buffer echoes."""
     from client import client
-    assert client(u'less') == u'less'
+    assert client('less') == 'less'
 
 
 def test_multiple():
     """Test if a string of multiple buffer lengths echoes."""
     from client import client
-    lots_of_words = u'this long string uses multiple buffer lengths'
+    lots_of_words = 'this long string uses multiple buffer lengths'
     assert client(lots_of_words) == lots_of_words
 
 
 def test_exact():
     """Test if a string that is an exact multiple of buffer length echoes."""
     from client import client
-    assert client(u'exactlyoneexactly') == u'exactlyoneexactly'
+    assert client('exactlyoneexactly') == 'exactlyoneexactly'
 
 
 def test_nonascii():
     """Test if a string of non-ascii characters echoes."""
     from client import client
-    assert client(u'ΞΞΞΞΞΞΞ') == u'ΞΞΞΞΞΞΞ'
+    assert client('ΞΞΞΞΞΞΞ') == 'ΞΞΞΞΞΞΞ'
